b48b7bc90a88cf557d7b4c7ee7cc02c2f9f931ee
Joe Cooper <joecooper2991@gmail.com>
2020-05-06 21:00:50 +0100

    add styling to tab bar

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { SafeAreaView, StatusBar, View } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 
@@ -6,16 +7,44 @@ import { createMaterialTopTabNavigator } from "@react-navigation/material-top-ta
 const Tab = createMaterialTopTabNavigator();
 
 import HomeScreen from "./src/screens/HomeScreen";
-import SettingsScreen from './src/screens/SettingsScreen';
+import SettingsScreen from "./src/screens/SettingsScreen";
 
 export default function App() {
+  //Set color of status bar elements
+  StatusBar.setBarStyle("light-content", true);
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator backBehaviour="order" >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name = "Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <>
+      {/*SafeAreaView sets color of statusbar for ios */}
+      <SafeAreaView style={{ backgroundColor: "black" }} />
+      {/*Sets color of statusbar for android */}
+      <View
+        height={StatusBar.currentHeight}
+        style={{ backgroundColor: "black", color: "white" }}
+      />
+      {/*Navigation */}
+      <NavigationContainer>
+        <Tab.Navigator
+          backBehaviour="order"
+          tabBarOptions={{
+            activeTintColor: "red",
+            inactiveTintColor: "grey",
+            style: { backgroundColor: "black" },
+            indicatorStyle: { backgroundColor: "black" },
+          }}
+        >
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ tabBarAccessibilityLabel: "Home" }}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={SettingsScreen}
+            options={{ tabBarAccessibilityLabel: "Home" }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
