fe3b35d51b4c30d2dca03619ab15c876b1e8b5cd
Joe Cooper <joecooper2991@gmail.com>
2020-05-06 21:54:44 +0100

    api call to spotify

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,12 @@
-import React from "react";
-import axios from 'axios';
+import axios from "axios";
+
+export const getTopTracks = () => {
+  return axios
+    .get(`https://api.spotify.com/v1`, {headers: {}})
+    .then((result) => {
+      return result.data;
+    })
+    .catch((err) => {
+      return "error";
+    });
+};
