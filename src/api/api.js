fc419b8012eeadc3c9354b0d6b0f31b9d0db5e42
Joe Cooper <joecooper2991@gmail.com>
2020-05-07 11:23:47 +0100

    getTopTracks function added

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+import { clientId, clientSecret } from "./auth";
+
 export const getTopTracks = () => {
   return axios
-    .get(`https://api.spotify.com/v1`, {headers: {}})
+    .get(`https://api.spotify.com/v1/search?q=bowie&type=artist`)
     .then((result) => {
       return result.data;
     })
     .catch((err) => {
-      return "error";
+      return err;
     });
 };
--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,12 +1,23 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View } from "react-native";
 
-import styles from '../assets/stylesheets/SettingsScreen';
+import styles from "../assets/stylesheets/SettingsScreen";
 
-export default function HomeScreen() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Settings</Text>
-      </View>
-    );
-  }
+import { getTopTracks } from "../api/api";
+
+export default function SettingsScreen() {
+  const [topTracks, setTopTracks] = useState([{ id: 5 }, { id: 6 }]);
+  useEffect(async () => {
+    const topTracks = await getTopTracks();
+    console.log(topTracks);
+  });
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Settings</Text>
+      {topTracks.map((track) => {
+        return <Text key={track.id}>{track.id}</Text>;
+      })}
+    </View>
+  );
+}
