b117c16d6560de0397e979fd16db69ec63289809
Joe Cooper <joecooper2991@gmail.com>
2020-05-07 15:18:31 +0100

    add get artist info api call

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+import { lastApiKey } from "./auth";
+
 export const getTopTracks = () => {
   return axios
     .get(`https://api.spotify.com/v1/search?q=bowie&type=artist`)
@@ -10,3 +12,20 @@ export const getTopTracks = () => {
       return err;
     });
 };
+
+export const getArtistInfo = (artist = "Cher") => {
+  return axios
+    .get(
+      `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artist}&api_key=${lastApiKey}&format=json`,
+      {},
+      { headers: { "User-Agent": "onlyReggae" } }
+    )
+    .then((result) => {
+      console.log(result.data.artist.bio.summary);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const getTopArtistsByGenre = () => {}
--- src/api/authorise.js
+++ src/api/authorise.js
@@ -1,34 +1,30 @@
-import axios from 'axios';
-import qs from 'qs';
+import axios from "axios";
+import qs from "qs";
 
-import {clientId, clientSecret} from './auth';
+//Get and encode into base64 id and secret
+import { clientId, clientSecret } from "./auth";
+import base64 from "react-native-base64";
 
 export const getAuth = async () => {
-  
-  
+  console.log("hi");
   const headers = {
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    auth: {
-      username: clientId,
-      password: clientSecret,
-    },
+    Authorization: `Basic ${base64.encode(`${clientId}:${clientSecret}`)}`,
+    "Content-Type": "application/x-www-form-urlencoded",
+    redirect_uri: 'http://localhost:8888/callback'
   };
   const data = {
-    grant_type: 'client_credentials',
+    grant_type: "client_credentials",
   };
 
   try {
     const response = await axios.post(
-      'https://accounts.spotify.com/api/token',
-      qs.stringify(data),
+      "https://accounts.spotify.com/api/token",
+      data,
       headers
     );
     console.log(response.data.access_token);
     return response.data.access_token;
   } catch (error) {
-    console.log(error);
+    console.log('ERROR', error);
   }
 };
