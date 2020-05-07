14df6f9cf83cb09797b9d5f489a9b3b635dc456d
Joe Cooper <joecooper2991@gmail.com>
2020-05-07 16:44:42 +0100

    artistcard directions changed

--- src/assets/stylesheets/ArtistCard.js
+++ src/assets/stylesheets/ArtistCard.js
@@ -5,7 +5,9 @@ const { width: winWidth, height: winHeight } = Dimensions.get("window");
 export const styleMaker = (props) => {
   const rank = props.artist["@attr"].rank;
   const bgColor = rank % 3 === 0 ? "green" : rank % 3 === 2 ? "yellow" : "red";
-  const txtColor = rank % 3 === 0 ? "white" : rank % 3 === 2 ? "black" : "black";
+  const txtColor =
+    rank % 3 === 0 ? "white" : rank % 3 === 2 ? "black" : "black";
+  const textPosition = rank % 2 === 1 ? "flex-start" : "flex-end";
 
   return StyleSheet.create({
     container: {
@@ -14,8 +16,10 @@ export const styleMaker = (props) => {
       backgroundColor: bgColor,
       marginTop: 30,
       color: txtColor,
-      display: 'flex',
+      display: "flex",
+      borderRadius: 20,
+      alignItems: textPosition,
     },
-    name: { color: txtColor },
+    name: { color: txtColor, margin: 20, fontSize: 17 },
   });
 };
