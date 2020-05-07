339aaf6de72a28f7ebae6be08a9d47e371072e77
Joe Cooper <joecooper2991@gmail.com>
2020-05-07 15:16:29 +0100

    revert settings back to just text

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -3,28 +3,13 @@ import { Text, View } from "react-native";
 
 import styles from "../assets/stylesheets/SettingsScreen";
 
-import { getTopTracks } from "../api/api";
+import { getTopTracks, getArtistInfo } from "../api/api";
 
 export default function SettingsScreen() {
-  //Set state
-  const [topTracks, setTopTracks] = useState([{ id: 5 }, { id: 6 }]);
-
-  //Get top tracks on component mounting
-  useEffect(() => {
-    async () => {
-      if (topTracks === []) {
-        const newTopTracks = await getTopTracks();
-        setTopTracks(newTopTracks);
-      }
-    };
-  });
 
   return (
     <View style={styles.container}>
       <Text style={styles.welcome}>Settings</Text>
-      {topTracks.map((track) => {
-        return <Text key={track.id}>{track.id}</Text>;
-      })}
     </View>
   );
 }
