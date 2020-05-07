1b707a567f00a73e3fb8f73afa4930a248b232ee
Joe Cooper <joecooper2991@gmail.com>
2020-05-07 17:45:18 +0100

    artistcard changes

--- src/components/ArtistCard.js
+++ src/components/ArtistCard.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
+import Button from "react-native-button";
 
-import {styleMaker} from "../assets/stylesheets/ArtistCard";
+import { styleMaker } from "../assets/stylesheets/ArtistCard";
 
 export default function ArtistCard(props) {
   const styles = styleMaker(props);
 
+  const goToPage = () => {
+    props.navigation.jumpTo("Artist", { artist: props.artist });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.name}>{props.artist.name}</Text>
-      
-    </View>
+    <Button onPress={goToPage}>
+      <View style={styles.container}>
+        <Text style={styles.name}>{props.artist.name}</Text>
+      </View>
+    </Button>
   );
-};
+}
