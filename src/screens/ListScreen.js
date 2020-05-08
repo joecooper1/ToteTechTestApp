79a79b8f9e4557c7dfc095b604fa26c73e1e3452
Joe Cooper <joecooper2991@gmail.com>
2020-05-08 22:01:11 +0100

    render scrollbars inside flatlist

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,11 +1,32 @@
 import React from "react";
-import { View, FlatList, Dimensions } from "react-native";
+import { Text, View, FlatList, Dimensions } from "react-native";
 
 import { getTopArtistsByGenre, getTopTracksByGenre } from "../api/api";
 
 import ScrollBar from "../components/ScrollBar";
 
 const { width: winWidth, height: winHeight } = Dimensions.get("window");
+
+const data = [
+  {
+    label: "Top Artists",
+    scrollThrough: getTopArtistsByGenre,
+    argument: "",
+    type: "Artist",
+  },
+  {
+    label: "Reggae Heartbeat",
+    scrollThrough: getTopTracksByGenre,
+    argument: "pp.53250712",
+    type: "Track",
+  },
+  {
+    label: "Dancehall",
+    scrollThrough: getTopTracksByGenre,
+    argument: "pp.182041037",
+    type: "Track",
+  },
+];
 
 export default function ListScreen({ navigation }) {
   return (
@@ -16,27 +37,22 @@ export default function ListScreen({ navigation }) {
         backgroundColor: "black",
       }}
     >
-        <ScrollBar
-          label="Top Artists"
-          scrollThrough={getTopArtistsByGenre}
-          argument={""}
-          navigation={navigation}
-          type="Artist"
-        />
-        <ScrollBar
-          label="Reggae HeartBeat"
-          scrollThrough={getTopTracksByGenre}
-          argument={"pp.53250712"}
-          navigation={navigation}
-          type="Track"
-        />
-        <ScrollBar
-          label="Dancehall"
-          scrollThrough={getTopTracksByGenre}
-          argument={"pp.182041037"}
-          navigation={navigation}
-          type="Track"
-        />
+      <FlatList
+        data={data}
+        renderItem={({ item, index, seperators }) => {
+          return (
+            <ScrollBar
+              label={item.label}
+              scrollThrough={item.scrollThrough}
+              argument={item.argument}
+              navigation={navigation}
+              type={item.type}
+              key={item.argument}
+            />
+          );
+        }}
+        style={{ width: winWidth, minHeight: winHeight }}
+      />
     </View>
   );
 }
