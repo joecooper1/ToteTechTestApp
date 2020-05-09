a96910253b64fe0e4767735514a729c09ccaf9e5
Joe Cooper <joecooper2991@gmail.com>
2020-05-09 16:49:32 +0100

    push data through homescreen to listscreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,6 +11,37 @@ import ArtistScreen from "./ArtistScreen";
 import TrackScreen from "./TrackScreen";
 import AlbumScreen from "./AlbumScreen";
 
+
+import { getTopArtistsByGenre, getTopTracksByGenre } from "../api/api";
+
+//Set initial data to loop through for flatlist
+const data = [
+  {
+    label: "Top Artists",
+    scrollThrough: getTopArtistsByGenre,
+    argument: "topartists",
+    type: "Artist",
+  },
+  {
+    label: "Reggae Heartbeat",
+    scrollThrough: getTopTracksByGenre,
+    argument: "pp.53250712",
+    type: "Track",
+  },
+  {
+    label: "Dancehall",
+    scrollThrough: getTopTracksByGenre,
+    argument: "pp.182041037",
+    type: "Track",
+  },
+  {
+    label: "1975",
+    scrollThrough: getTopTracksByGenre,
+    argument: "pp.160452881",
+    type: "Track",
+  },
+];
+
 export default function HomeScreen({ navigation }) {
   return (
     <Stack.Navigator
@@ -26,9 +57,10 @@ export default function HomeScreen({ navigation }) {
     >
       <Stack.Screen
         name="List"
-        component={ListScreen}
         options={{ title: "", headerStyle: { height: 0 } }}
-      />
+      >
+        {(props) => <ListScreen {...props} data={data} />}
+      </Stack.Screen>
       <Stack.Screen
         name="Artist"
         component={ArtistScreen}
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -7,35 +7,9 @@ import ScrollBar from "../components/ScrollBar";
 
 const { width: winWidth, height: winHeight } = Dimensions.get("window");
 
-//Set initial data to loop through for flatlist
-const data = [
-  {
-    label: "Top Artists",
-    scrollThrough: getTopArtistsByGenre,
-    argument: "topartists",
-    type: "Artist",
-  },
-  {
-    label: "Reggae Heartbeat",
-    scrollThrough: getTopTracksByGenre,
-    argument: "pp.53250712",
-    type: "Track",
-  },
-  {
-    label: "Dancehall",
-    scrollThrough: getTopTracksByGenre,
-    argument: "pp.182041037",
-    type: "Track",
-  },
-  {
-    label: "1975",
-    scrollThrough: getTopTracksByGenre,
-    argument: "pp.160452881",
-    type: "Track",
-  },
-];
 
-export default function ListScreen({ navigation }) {
+
+export default function ListScreen({ navigation, data }) {
   const [searchResults, setSearchResults] = useState(data);
 
   return (
