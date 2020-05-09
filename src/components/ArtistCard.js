77467dfe835f747271f204e8c076c4798e4a4a0d
Joe Cooper <joecooper2991@gmail.com>
2020-05-09 20:38:29 +0100

    added album art to artist cards

--- src/components/ArtistCard.js
+++ src/components/ArtistCard.js
@@ -4,16 +4,24 @@ import Button from "react-native-button";
 
 import { styleMaker } from "../assets/stylesheets/ArtistCard";
 
-import { getImageUrl } from "../api/api";
+import { getImageUrl, getAlbumImageUrl } from "../api/api";
 
 export default function ArtistCard(props) {
   const [imageUri, setImageUri] = useState(null);
 
   useEffect(() => {
+    //If element has artistId, use this to get image, else use Id
     const getUrl = async () => {
-      const artistId = props.element.artistId ? props.element.artistId : props.element.id;
-      const newUrl = await getImageUrl(artistId);
-      newUrl ? setImageUri(newUrl) : setImageUri('none');
+      if (props.element.id.match(/alb/g)) {
+        const newUrl = await getAlbumImageUrl(props.element.id);
+        newUrl ? setImageUri(newUrl) : setImageUri("none");
+      } else {
+        const artistId = props.element.artistId
+          ? props.element.artistId
+          : props.element.id;
+        const newUrl = await getImageUrl(artistId);
+        newUrl ? setImageUri(newUrl) : setImageUri("none");
+      }
     };
     if (!imageUri) getUrl();
   }, [props.element.name]);
@@ -24,20 +32,26 @@ export default function ArtistCard(props) {
     props.navigation.navigate(props.type, { element: props.element });
   };
 
+  //Check if element has artistName, and if it does render it
   const artistName = props.element.artistName ? (
     <Text style={{ color: "rgba(0, 0, 0, 0.6)" }}>
       {props.element.artistName}
     </Text>
   ) : null;
 
+  //Shorten name of song/artist if too long
+  const name = props.element.name
+    .slice(0, 30)
+    .concat(props.element.name.length > 30 ? "..." : "");
+
   return (
     <Button onPress={goToPage}>
       <View style={styles.container}>
         <View style={styles.name}>
-          <Text>{props.element.name}</Text>
+          <Text>{name}</Text>
           {artistName}
         </View>
-        <Image style={styles.image} source={{uri: imageUri}}/>
+        <Image style={styles.image} source={{ uri: imageUri }} />
       </View>
     </Button>
   );
