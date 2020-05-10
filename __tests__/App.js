b7560192b98e2a1848ea10d4b9d386d033b30293
Joe Cooper <joecooper2991@gmail.com>
2020-05-10 10:53:09 +0100

    more api tests

--- __tests__/App.js
+++ __tests__/App.js
@@ -14,16 +14,41 @@ import * as api from "../src/api/api";
 
 describe("api tests", () => {
   it("getTopTracksByPlaylist returns array of ten tracks when given a playlist id", (done) => {
-    api.getTopTracksByGenre("pp.53250712").then((result) => {
+    api.getTopTracksByPlaylist("pp.53250712").then((result) => {
       expect(result.length).toBe(10);
       expect(result[0].name).toBe("She's Royal");
       done();
     });
   });
   it("getTopArtistsByGenre returns array of ten artists", (done) => {
-    api.getTopTracksByGenre().then((result) => {
+    api.getTopArtistsByGenre().then((result) => {
       expect(result.length).toBe(10);
-      expect(result[0].name).toBe("She's Royal");
+      expect(result[0].name).toBe("Bob Marley");
+      done();
+    });
+  });
+  it("getTopTracksByArtist returns array of ten tracks when given artist id", (done) => {
+    api.getTopTracksByArtist("art.4771").then((result) => {
+      expect(result.length).toBe(10);
+      expect(result[0].name).toBe("Believe");
+      done();
+    });
+  });
+  it("getTracksByAlbum returns array of tracks when given album id", (done) => {
+    api.getTracksByAlbum("alb.54719066").then((result) => {
+      expect(result[0].name).toBe("Over My Dead Body (Album Version (Edited))");
+      done();
+    });
+  });
+  it("searchByTerm returns array of artists when given keyword and type artist", (done) => {
+    api.searchByTerm({term: "Cher", type: 'artist'}).then((result) => {
+      expect(result[0].name).toBe("Cher");
+      done();
+    });
+  });
+  it("searchByTerm returns array of tracks when given keyword and type track", (done) => {
+    api.searchByTerm({term: "Cher", type: 'track'}).then((result) => {
+      expect(result[0].name).toBe("Believe");
       done();
     });
   });
