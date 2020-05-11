1834ff5f662ead2741524f0377c797bb225d1b5c
Joe Cooper <joecooper2991@gmail.com>
2020-05-11 09:15:34 +0100

    change auth page

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,3 +1,2 @@
-export const lastApiKey = '00f3c1ddce55465fb087418a70073bf3';
-export const lastApiSecret = '0204a99c11ddaefec7bf6b721b28a869';
+
 export const napApiKey = 'NTM4YWQwOGYtNzczNy00NDUxLTk0ZGEtNTcwYTNiYjAxOTE1';
